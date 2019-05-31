c0b280c4574e17e4dee171c6e99ea56b8a587c2f
msunkist99 <msunkist99@gmail.com>
2019-05-31 18:49:32 -0500

    2019/05/31 mock ViewModel data for StLouisSites index view

--- MVCStLouisSites/Controllers/StLouisSitesController.cs
+++ MVCStLouisSites/Controllers/StLouisSitesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MVCStLouisSites.ViewModels.StLouisSites;
 
 namespace MVCStLouisSites.Controllers
 {
@@ -10,7 +11,9 @@ namespace MVCStLouisSites.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            List<StLouisSitesIndexViewModel> models = StLouisSitesIndexViewModel.GetStLouisSitesIndexViewModel();
+
+            return View(models);
         }
     }
 }
